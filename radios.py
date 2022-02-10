74d6f042c4d35bd079b742935fb492160df03cbf
Due204 <due204@gmail.com>
2022-02-10 01:33:18 -0300

    Update radios.py

--- radios.py
+++ radios.py
@@ -4,7 +4,7 @@ radio = {
     "La 100": "https://cdns-fm99956-1.planisys.net/hls/chunklist.m3u8",
     "Pop radio 101.5": "https://ny2.stweb.tv/popradio/live/chunks.m3u8?nimblesessionid=76280328",
     "Rock&Pop": "https://25023.live.streamtheworld.com/ROCKANDPOPAAC.aac",
-    "Mega98.3": "https://mega.stweb.tv/mega983/live/l_13_451404008_6123.aac?nimblesessionid=355939424",
+    "Mega98.3": "https://mega.stweb.tv/mega983/live/chunks.m3u8",
     "Radio Disney 94.3": "https://14083.live.streamtheworld.com/DISNEY_ARG_BA_SC",
     "Vale 97.5": "https://s6.stweb.tv/vale/live/chunks.m3u8?nimblesessionid=79243779",
     "Los 40 Principales": "https://21933.live.streamtheworld.com/LOS40_ARGENTINA_SC",
