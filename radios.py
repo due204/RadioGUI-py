89f32b128b965902ac9148770844e60fb1c27f6e
Due204 <due204@gmail.com>
2022-02-10 01:16:10 -0300

    Update radios.py
    Agregadas algunas radios populares de Argentina.

--- radios.py
+++ radios.py
@@ -1,1 +1,12 @@
-radio = {'Anime Nexus': 'http://radio.animenexus.mx:8000/animenexus', 'Vale 97.1': 'http://vale.stweb.tv:1935/vale/live/playlist.m3u8'}
+radio = {
+    "Anime Nexus": "http://radio.animenexus.mx:8000/animenexus",
+    "Vale 97.1": "http://vale.stweb.tv:1935/vale/live/playlist.m3u8",
+    "La 100": "https://cdns-fm99956-1.planisys.net/hls/chunklist.m3u8",
+    "Pop radio 101.5": "https://ny2.stweb.tv/popradio/live/chunks.m3u8?nimblesessionid=76280328",
+    "Rock&Pop": "https://25023.live.streamtheworld.com/ROCKANDPOPAAC.aac",
+    "Mega98.3": "https://mega.stweb.tv/mega983/live/l_13_451404008_6123.aac?nimblesessionid=355939424",
+    "Radio Disney 94.3": "https://14083.live.streamtheworld.com/DISNEY_ARG_BA_SC",
+    "Vale 97.5": "https://s6.stweb.tv/vale/live/chunks.m3u8?nimblesessionid=79243779",
+    "Los 40 Principales": "https://21933.live.streamtheworld.com/LOS40_ARGENTINA_SC",
+    "Aspen 102.3": "https://mdstrm.com/audio/60a2745ff943100826374a70/icecast.audio",
+}
