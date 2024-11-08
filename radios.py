b18ab775c39f16eefeb21d01b80909e4a4ec2b7c
Due204 <due204@gmail.com>
2024-11-08 20:04:15 -0300

    Update radios.py
    Radios actualizadas

--- radios.py
+++ radios.py
@@ -2,15 +2,4 @@
 # Se guardan primero el nombre, dos puntos (:) y por ultimo el link de la radio
 # Ejemplo:
 # "Nombre de la radio" : "link_de_la_radio"
-radio = {
-    "Anime Nexus": "http://radio.animenexus.mx:8000/animenexus",
-    "Vale 97.1": "http://vale.stweb.tv:1935/vale/live/playlist.m3u8",
-    "La 100": "https://cdns-fm99956-1.planisys.net/hls/chunklist.m3u8",
-    "Pop radio 101.5": "https://ny2.stweb.tv/popradio/live/chunks.m3u8?nimblesessionid=76280328",
-    "Rock&Pop": "https://25023.live.streamtheworld.com/ROCKANDPOPAAC.aac",
-    "Mega98.3": "https://mega.stweb.tv/mega983/live/chunks.m3u8",
-    "Radio Disney 94.3": "https://14083.live.streamtheworld.com/DISNEY_ARG_BA_SC",
-    "Vale 97.5": "https://s6.stweb.tv/vale/live/chunks.m3u8?nimblesessionid=79243779",
-    "Los 40 Principales": "https://21933.live.streamtheworld.com/LOS40_ARGENTINA_SC",
-    "Aspen 102.3": "https://mdstrm.com/audio/60a2745ff943100826374a70/icecast.audio",
-}
+radio = {'Vorterix': 'https://ice2.edge-apps.net/radio1_high-20057.audio', 'Vale 97.1': 'http://vale.stweb.tv:1935/vale/live/playlist.m3u8', 'Mega98.3': 'https://mega.stweb.tv/mega983/live/chunks.m3u8', 'Los 40 Principales': 'https://edge05.radiohdvivo.com/stream/los40', 'Aspen 102.3': 'https://27373.live.streamtheworld.com/ASPEN.mp3', 'Anime Nexus': 'http://radio.animenexus.mx:8000/animenexus'}
